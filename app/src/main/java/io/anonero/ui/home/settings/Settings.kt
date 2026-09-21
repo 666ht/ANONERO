8a0c3122c7c0edaf00fee273eb528ece7964a56e
666ht <995749039@qq.com>
2026-09-21 11:15:01 +0800

    fix: translate remaining Chinese UI text

--- app/src/main/java/io/anonero/ui/home/settings/Settings.kt
+++ app/src/main/java/io/anonero/ui/home/settings/Settings.kt
@@ -132,7 +132,7 @@ fun SettingsPage(
                            )
                        }
                    }catch (e: Exception) {
-                       toastState.show("Error changing PIN. Check logs for more details",
+                       toastState.show("修改 PIN 码失败，请查看日志了解详情",
                            type = ToastType.Error
                        )
                        Timber.tag(TAG).e(e)
