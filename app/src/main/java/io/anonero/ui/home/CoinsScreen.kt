4374a3e03fb0a9b4d27d8e10c54c0dee0d0190bf
666ht <995749039@qq.com>
2026-09-21 11:43:13 +0800

    fix: translate remaining user-facing UI text

--- app/src/main/java/io/anonero/ui/home/CoinsScreen.kt
+++ app/src/main/java/io/anonero/ui/home/CoinsScreen.kt
@@ -139,7 +139,7 @@ fun CoinsScreen(
         topBar = {
             TopAppBar(
                 title = {
-                    Text("Coins")
+                    Text("零钱")
                 },
                 navigationIcon = {
                     IconButton(
