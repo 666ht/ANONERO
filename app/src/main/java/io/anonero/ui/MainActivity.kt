ab3de704a4ae1c88880ccf8cb7faeb8bbca36b00
666ht <995749039@qq.com>
2026-09-21 12:23:47 +0800

    fix: import Compose crash dialog components

--- app/src/main/java/io/anonero/ui/MainActivity.kt
+++ app/src/main/java/io/anonero/ui/MainActivity.kt
@@ -20,6 +20,9 @@ import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
