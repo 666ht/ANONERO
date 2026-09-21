c1e3d5ff0bc388a320a9f76e99c1560c207a1ecb
666ht <995749039@qq.com>
2026-09-21 12:17:29 +0800

    fix: package prebuilt native libraries without CMake build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,6 @@ android {
             useSupportLibrary = true
         }
         ndk {
-            //noinspection ChromeOsAbiSupport
             abiFilters += listOf("armeabi-v7a", "arm64-v8a")
         }
         packaging {
@@ -48,32 +47,21 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlin {
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
+
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-
-    externalNativeBuild {
-        cmake {
-            path = file("CMakeLists.txt")
-        }
-    }
-
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-            excludes += "META-INF/versions/9/OSGI-INF/MANIFEST.MF"
-        }
-
     }
 
     flavorDimensions += "anon_mode"
@@ -92,15 +80,13 @@ android {
             dimension = "anon_mode"
             buildConfigField("String", "FLAVOR", "\"nero\"")
             buildConfigField("boolean", "VIEW_ONLY", "true")
-        }
-
+            }
     }
-    buildToolsVersion = "36.0.0"
+
     ndkVersion = "29.0.13599879"
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -110,42 +96,33 @@ dependencies {
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.runtime.livedata)
     implementation(libs.androidx.compose.animation)
-
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.datastore.preferences)
-
-
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.datastore.preferences.core.jvm)
-
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.camera.view)
-
     implementation(platform(libs.koin.bom))
     implementation(libs.koin.core)
     implementation(libs.koin.core.coroutines)
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.navigation)
     implementation(libs.koin.androidx.compose)
-
     implementation(libs.org.jetbrains.kotlin.android)
-
     implementation(libs.org.boofcv.boofcv.core)
     implementation(libs.com.google.zxing.core)
     implementation(libs.com.google.accompanist.permissions)
-
     implementation(libs.com.jakewharton.timber)
     implementation(libs.org.bouncycastle.bcprov)
     implementation(libs.com.sparrowwalle.hummingbird)
-
     implementation(libs.io.matthewnelson.kmp.tor.runtime)
     implementation(libs.io.matthewnelson.kmp.resource.exec.tor)
     implementation(libs.com.sparrowwallet.hummingbird)
@@ -154,14 +131,11 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation("dev.chrisbanes.haze:haze:1.6.10")
     implementation("androidx.compose.material:material-icons-extended")
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
 }
