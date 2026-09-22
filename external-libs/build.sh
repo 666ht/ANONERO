eb4dbe42fdbd9cdb09192862742452c2b56c997a
666ht <995749039@qq.com>
2026-09-22 23:44:26 +0800

    fix: build wallet API archive for Android

--- external-libs/build.sh
+++ external-libs/build.sh
@@ -163,6 +163,10 @@ step_monero() {
       CMAKE_LIBRARY_PATH="$PREFIX/lib" ANDROID_STANDALONE_TOOLCHAIN_PATH= \
       ANDROID_NDK_ROOT="$NDK" USE_SINGLE_BUILDDIR=1 \
       make "$MONERO_TARGET" -j"$NPROC"
+    # The Android release target does not necessarily build the wallet API
+    # static archive. Build it explicitly because the JNI layer links against it.
+    cmake --build "$MONERO/build/release" --target wallet_api -j"$NPROC"
+    test -s "$MONERO/build/release/src/wallet/api/libwallet_api.a"
   else
     mkdir -p build/release && cd build/release
     cmake -D CMAKE_BUILD_TYPE=Release -D STATIC=OFF -D ARCH="$MONERO_ARCH" -D BUILD_64=ON -D BUILD_TESTS=OFF -D BUILD_GUI_DEPS=1 -D USE_DEVICE_TREZOR=OFF -D STACK_TRACE=OFF -D CMAKE_POSITION_INDEPENDENT_CODE=ON -D BUILD_TAG="linux-x64" -D CMAKE_PREFIX_PATH="$PREFIX" -D BOOST_ROOT="$PREFIX" -D BOOST_IGNORE_SYSTEM_PATHS=ON -D OPENSSL_ROOT_DIR="$PREFIX" ../..
