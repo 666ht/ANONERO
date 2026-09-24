7dc5fbac1095c95c94692c3a35cbcc1aaace1d23
666ht <995749039@qq.com>
2026-09-25 05:39:03 +0800

    Show full transaction date and time with seconds

--- app/src/main/java/io/anonero/ui/home/TransactionDetail.kt
+++ app/src/main/java/io/anonero/ui/home/TransactionDetail.kt
@@ -325,7 +325,7 @@ fun TransactionDetailScreen(
                         title = stringResource(R.string.time),
                         subtitle = Formats.formatTransactionTime(
                             transactionInfo!!.timestamp,
-                            "HH:mm dd/MM/yyy"
+                            "yyyy/MM/dd HH:mm:ss"
                         )
                     )
                 }
