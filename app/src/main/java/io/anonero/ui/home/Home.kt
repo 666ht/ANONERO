943c8dafb8f7fc96b698fabf319916e76a0eb984
666ht <995749039@qq.com>
2026-09-25 03:24:41 +0800

    将硬编码 UI 文案改为翻译资源

--- app/src/main/java/io/anonero/ui/home/Home.kt
+++ app/src/main/java/io/anonero/ui/home/Home.kt
@@ -130,7 +130,7 @@ fun HomeScreenComposable(modifier: Modifier = Modifier, mainNavController: NavHo
             },
             text = {
                 Text(
-                    "In order for ΛИ0ИΞR0 to function properly in the background, it is recommended to disable battery optimization.\\nYou can change this setting anytime in Android's app settings.\\n\\nWould you like to proceed?"
+                    stringResource(R.string.battery_optimization_message)
                 )
             },
             confirmButton = {
