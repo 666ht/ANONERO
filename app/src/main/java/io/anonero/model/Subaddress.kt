b4d42ac4a56ed504e4ed2e7f5ed92b2ca38f0130
666ht <995749039@qq.com>
2026-09-25 06:41:42 +0800

    Remove # from all subaddress labels

--- app/src/main/java/io/anonero/model/Subaddress.kt
+++ app/src/main/java/io/anonero/model/Subaddress.kt
@@ -36,9 +36,11 @@ class Subaddress(
         get() = address.substring(0, 8) + "…" + address.substring(address.length - 8)
 
     val displayLabel: String
-        get() = if (label.isEmpty() || DEFAULT_LABEL_FORMATTER.matcher(label)
-                .matches()
-        ) "SubAddress $addressIndex" else label
+        get() = (if (label.isEmpty() || DEFAULT_LABEL_FORMATTER.matcher(label).matches()) {
+            "SubAddress $addressIndex"
+        } else {
+            label
+        }).replace("#", "")
 
     companion object {
         val DEFAULT_LABEL_FORMATTER: Pattern =
@@ -47,6 +49,4 @@ class Subaddress(
 
     val totalAmount: Long
         get() = amount
-
-
 }
