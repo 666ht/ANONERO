f49421ed8dde35f07e032545250cd9bd0e26bd32
666ht <995749039@qq.com>
2026-09-25 11:39:29 +0800

    fix: show primary address as 0

--- app/src/main/java/io/anonero/model/Subaddress.kt
+++ app/src/main/java/io/anonero/model/Subaddress.kt
@@ -37,7 +37,7 @@ class Subaddress(
 
     val displayLabel: String
         get() = (if (label.isEmpty() || DEFAULT_LABEL_FORMATTER.matcher(label).matches()) {
-            if (addressIndex == 0) "主地址" else "子地址 $addressIndex"
+            if (addressIndex == 0) "主地址 0" else "子地址 $addressIndex"
         } else {
             label
         }).replace("#", "")
