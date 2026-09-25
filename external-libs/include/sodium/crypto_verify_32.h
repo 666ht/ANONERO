eebe6270ab5fbbd526bd431e5d46401a25d9c5ed
666ht <995749039@qq.com>
2026-09-26 05:56:14 +0800

    Fix Android APK libsodium crypto_verify_32 header

---
+++ external-libs/include/sodium/crypto_verify_32.h
@@ -0,0 +1,18 @@
+#ifndef crypto_verify_32_H
+#define crypto_verify_32_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define crypto_verify_32_BYTES 32U
+
+int crypto_verify_32(const unsigned char *x, const unsigned char *y);
+
+#define crypto_verify_32_ref crypto_verify_32
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
