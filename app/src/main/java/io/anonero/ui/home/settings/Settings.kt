fc45c310daafe9b51c209d3043c702b77c659220
666ht <995749039@qq.com>
2026-09-25 08:14:16 +0800

    Fix PIN callback resource compilation

--- app/src/main/java/io/anonero/ui/home/settings/Settings.kt
+++ app/src/main/java/io/anonero/ui/home/settings/Settings.kt
@@ -116,7 +116,7 @@ fun SettingsPage(
                 onNext = {
                    try {
                        WalletManager.instance?.wallet?.setPassword(it)
-                       toastState.show(pinChangedSuccessfully,
+                       toastState.show(stringResource(R.string.pin_changed_successfully),
                            type = ToastType.Success,
                        )
                        prefs.edit(commit = true) {
@@ -132,7 +132,7 @@ fun SettingsPage(
                            )
                        }
                    }catch (e: Exception) {
-                       toastState.show(pinChangeError,
+                       toastState.show(stringResource(R.string.pin_change_error),
                            type = ToastType.Error
                        )
                        Timber.tag(TAG).e(e)
