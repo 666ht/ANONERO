d741b9b7a7531908e3ee29d23a0717c2707f23a6
666ht <995749039@qq.com>
2026-09-26 06:06:33 +0800

    Restore complete libsodium crypto verify header

--- external-libs/include/sodium/crypto_verify_32.h
+++ external-libs/include/sodium/crypto_verify_32.h
@@ -1,15 +1,20 @@
 #ifndef crypto_verify_32_H
 #define crypto_verify_32_H
 
+#include <stddef.h>
+#include "export.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #define crypto_verify_32_BYTES 32U
+SODIUM_EXPORT
+size_t crypto_verify_32_bytes(void);
 
-int crypto_verify_32(const unsigned char *x, const unsigned char *y);
-
-#define crypto_verify_32_ref crypto_verify_32
+SODIUM_EXPORT
+int crypto_verify_32(const unsigned char *x, const unsigned char *y)
+            __attribute__ ((warn_unused_result)) __attribute__ ((nonnull));
 
 #ifdef __cplusplus
 }
