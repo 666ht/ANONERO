98e53a8f55f2719a1c8838fc5ddb55e3313203d8
666ht <995749039@qq.com>
2026-09-25 09:13:45 +0800

    将默认子地址标签改为添加标签并保留编号

--- app/src/main/java/io/anonero/model/Subaddress.kt
+++ app/src/main/java/io/anonero/model/Subaddress.kt
@@ -37,7 +37,7 @@ class Subaddress(
 
     val displayLabel: String
         get() = (if (label.isEmpty() || DEFAULT_LABEL_FORMATTER.matcher(label).matches()) {
-            "子地址 $addressIndex"
+            "添加标签 $addressIndex"
         } else {
             label
         }).replace("#", "")
