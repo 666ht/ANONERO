0dc778d1cadbf8f436feabb4ca3772319c9bf70c
666ht <995749039@qq.com>
2026-09-26 07:36:15 +0800

    Fix CoinDetail Material3 experimental API build error

--- app/src/main/java/io/anonero/ui/home/CoinDetail.kt
+++ app/src/main/java/io/anonero/ui/home/CoinDetail.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -31,6 +32,7 @@ import io.anonero.services.WalletState
 import io.anonero.util.Formats
 import org.koin.compose.koinInject
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CoinDetailScreen(
     keyImage: String,
