24171017ea3fc8536a86139fc762fb496e51d440
666ht <995749039@qq.com>
2026-09-26 03:05:39 +0800

    Fix missing Compose layout imports

--- app/src/main/java/io/anonero/ui/home/TransactionDetail.kt
+++ app/src/main/java/io/anonero/ui/home/TransactionDetail.kt
@@ -9,6 +9,9 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
