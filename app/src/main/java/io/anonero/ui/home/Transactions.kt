1c6384e32996cc57e16735b4456762ef111752a8
666ht <995749039@qq.com>
2026-09-26 01:54:40 +0800

    Reduce transaction list arrow size

--- app/src/main/java/io/anonero/ui/home/Transactions.kt
+++ app/src/main/java/io/anonero/ui/home/Transactions.kt
@@ -770,7 +770,7 @@ fun TransactionItem(tx: TransactionInfo, hideAmounts: Boolean = false, modifier:
                 if (confirmations >= 10)
                     Icon(
                         if (isIncoming) AnonIcons.ArrowDownLeft else AnonIcons.ArrowUpRight,
-                        modifier = Modifier.size(32.dp),
+                        modifier = Modifier.size(26.dp),
                         tint = if (isIncoming) MaterialTheme.colorScheme.primary else LocalContentColor.current,
                         contentDescription = ""
                     )
