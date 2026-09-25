c2bedbdd300afd9396e9d6457824b43cc4024e93
666ht <995749039@qq.com>
2026-09-26 00:49:46 +0800

    Remove incorrect transaction error snackbar

--- app/src/main/java/io/anonero/ui/home/Send.kt
+++ app/src/main/java/io/anonero/ui/home/Send.kt
@@ -290,15 +290,8 @@ fun SendScreen(
     val walletState: WalletState by inject(WalletState::class.java)
 
     val showIndefiniteLoading by walletState.isLoading.asLiveData().observeAsState(false)
-\n    LaunchedEffect(txComposeError) {
-        txComposeError?.let { error ->
-            snackbarHostState.showSnackbar(error)
-        }
-    }
-
-
-
-    val unLockedAmount = Formats.getDisplayAmount(
+\n
+val unLockedAmount = Formats.getDisplayAmount(
         unlockedBalance ?: 0L
     )
 
