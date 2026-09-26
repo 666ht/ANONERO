b3fec8e75a214acf47b13b8d1ed758fcab8a2c1e
666ht <995749039@qq.com>
2026-09-26 23:54:09 +0800

    Restore previous NDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,7 +104,7 @@ android {
 
     }
     buildToolsVersion = "36.0.0"
-    ndkVersion = "27.2.12479018"
+    ndkVersion = "29.0.14206865"
 
     sourceSets["main"].res.srcDir("$buildDir/generated/xmrFontRes")
 }
