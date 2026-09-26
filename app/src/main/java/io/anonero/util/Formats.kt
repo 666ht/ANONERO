50b7bf81e2665d5401aa121ea2cca1d1c6a11c35
666ht <995749039@qq.com>
2026-09-26 12:25:55 +0800

    调整交易时间为中国日期格式

--- app/src/main/java/io/anonero/util/Formats.kt
+++ app/src/main/java/io/anonero/util/Formats.kt
@@ -34,7 +34,7 @@ object Formats {
         return d.toPlainString()
     }
 
-    fun formatTransactionTime(timestamp: Long, pattern: String = "HH:mm\ndd/MM"): String {
+    fun formatTransactionTime(timestamp: Long, pattern: String = "HH:mm\nMM月dd日"): String {
         val instant =
             Instant.ofEpochSecond(timestamp)
         val dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
