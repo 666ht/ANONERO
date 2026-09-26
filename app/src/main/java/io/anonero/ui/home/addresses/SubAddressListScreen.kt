42bd4e580eb48fad2316e4553d116f34f3d271fa
666ht <995749039@qq.com>
2026-09-26 10:06:37 +0800

    标签页列表金额下移与地址对齐

--- app/src/main/java/io/anonero/ui/home/addresses/SubAddressListScreen.kt
+++ app/src/main/java/io/anonero/ui/home/addresses/SubAddressListScreen.kt
@@ -115,28 +115,30 @@ fun SubAddressesScreen(
                                 navigateToDetails(address)
                             },
                         headlineContent = {
+                            Text(
+                                text = if (address.addressIndex == 0) "主地址 0" else address.displayLabelWithIndex,
+                                color = MaterialTheme.colorScheme.primary,
+                            )
+                        },
+                        supportingContent = {
                             Row(
                                 horizontalArrangement = Arrangement.SpaceBetween,
                                 modifier = Modifier.fillMaxWidth()
                             ) {
-                                Text(
-                                    text = if (address.addressIndex == 0) "主地址 0" else address.displayLabelWithIndex,
-                                    color = MaterialTheme.colorScheme.primary,
-                                )
+                                SelectionContainer {
+                                    Text(
+                                        address.squashedAddress,
+                                        fontWeight = FontWeight.SemiBold,
+                                        fontSize = 14.sp,
+                                        textAlign = TextAlign.Justify,
+                                        modifier = Modifier.weight(1f)
+                                    )
+                                }
                                 Text(
                                     Formats.getDisplayAmount(address.totalAmount),
                                     color = MaterialTheme.colorScheme.primary,
-                                    fontWeight = FontWeight.SemiBold
-                                )
-                            }
-                        },
-                        supportingContent = {
-                            SelectionContainer {
-                                Text(
-                                    address.squashedAddress,
                                     fontWeight = FontWeight.SemiBold,
-                                    fontSize = 14.sp,
-                                    textAlign = TextAlign.Justify
+                                    modifier = Modifier.padding(start = 8.dp)
                                 )
                             }
                         },
